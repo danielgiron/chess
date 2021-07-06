dd134f48ec85bc2902a00fe6abfd0c3698baebbb
danielgiron <73446404+danielgiron@users.noreply.github.com>
2021-07-06 05:35:55 -0400

    Board Initialization + Selection
    The board layout is initialized and drawn. Individual cells may be selected by unique id on click.
    
    Next step likely setting up starting position of all pieces.

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('chess');
+//selectors
+boardContainer = document.querySelector(".board__container");
+
+////initializing board
+let board = [];
+
+for (let i = 0; i < 8; i++) {
+  let row = [];
+  for (let n = 0; n < 8; n++) {
+    let cell = "" + i + n;
+    row.push(cell);
+  }
+  board.push(row);
+}
+
+board.forEach((row) => {
+  row.forEach((cell) => {
+    const newBlock = document.createElement("div");
+    newBlock.classList.add("gridBlock");
+    //console.log(cell);
+    newBlock.id = cell;
+    boardContainer.appendChild(newBlock);
+  });
+  const br = document.createElement("br");
+  boardContainer.appendChild(br);
+});
+////end of inititializing board
+
+//to select cell on boardContainer
+boardContainer.onclick = (e) => {
+  console.log(e.target.id); // get the element by id
+  var cell = document.getElementById(e.target.id);
+  try {
+    if (cell.innerText === "") {
+      cell.innerText = "♕";
+    } else {
+      cell.innerText = "";
+    }
+  } catch {
+    //not a cell
+  }
+};
