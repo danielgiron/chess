96004fcbec5d9632c4782a0928a4a9c5f268f877
danielgiron <73446404+danielgiron@users.noreply.github.com>
2021-07-08 02:32:10 -0400

    Cells Iabeled with Unique IDs

--- script.js
+++ script.js
@@ -13,56 +13,73 @@ for (let i = 0; i < 8; i++) {
   board.push(row);
 }
 
+//create all 64 cell divs to make up the board and add "cell" to their classlist
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let alpha = 0; //index variable of letters[]
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let num = 0; //index variable of numbers[]
+
+let cellFlip = true; //used to create alternating cells classed as "even" or not
 board.forEach((row) => {
   row.forEach((cell) => {
     const newBlock = document.createElement("div");
-    newBlock.classList.add("gridBlock");
-    //console.log(cell);
-    newBlock.id = cell;
+    newBlock.classList.add("cell");
+    if (cellFlip) {
+      newBlock.classList.add("even");
+    }
+    cellFlip = !cellFlip;
+    cellId = "" + letters[alpha] + numbers[num];
+    newBlock.id = cellId;
     boardContainer.appendChild(newBlock);
+
+    num = num + 1;
   });
   const br = document.createElement("br");
   boardContainer.appendChild(br);
+  cellFlip = !cellFlip;
+  num = 0;
+  alpha = alpha + 1;
 });
 
+//dictionary containing all pieces and their starting cells as key value pairs
 let pieces = {
-  blackTower1: "00",
-  blackTower2: "07",
-  blackHorse1: "01",
-  blackHorse2: "06",
-  blackBishop1: "02",
-  blackBishop2: "05",
-  blackKing: "04",
-  blackQueen: "03",
-  blackPawn1: "10",
-  blackPawn2: "11",
-  blackPawn3: "12",
-  blackPawn4: "13",
-  blackPawn5: "14",
-  blackPawn6: "15",
-  blackPawn7: "16",
-  blackPawn8: "17",
-  whiteTower1: "70",
-  whiteTower2: "77",
-  whiteHorse1: "71",
-  whiteHorse2: "76",
-  whiteBishop1: "72",
-  whiteBishop2: "75",
-  whiteKing: "74",
-  whiteQueen: "73",
-  whitePawn1: "60",
-  whitePawn2: "61",
-  whitePawn3: "62",
-  whitePawn4: "63",
-  whitePawn5: "64",
-  whitePawn6: "65",
-  whitePawn7: "66",
-  whitePawn8: "67",
+  blackTower1: "a1",
+  blackTower2: "a8",
+  blackKnight1: "a2",
+  blackKnight2: "a7",
+  blackBishop1: "a3",
+  blackBishop2: "a6",
+  blackKing: "a5",
+  blackQueen: "a4",
+  blackPawn1: "b1",
+  blackPawn2: "b2",
+  blackPawn3: "b3",
+  blackPawn4: "b4",
+  blackPawn5: "b5",
+  blackPawn6: "b6",
+  blackPawn7: "b7",
+  blackPawn8: "b8",
+  whiteTower1: "h1",
+  whiteTower2: "h8",
+  whiteHorse1: "h2",
+  whiteHorse2: "h7",
+  whiteBishop1: "h3",
+  whiteBishop2: "h6",
+  whiteKing: "h5",
+  whiteQueen: "h4",
+  whitePawn1: "g1",
+  whitePawn2: "g2",
+  whitePawn3: "g3",
+  whitePawn4: "g4",
+  whitePawn5: "g5",
+  whitePawn6: "g6",
+  whitePawn7: "g7",
+  whitePawn8: "g8",
 };
 
+//places all pieces in their initial cells
 function setup() {
   for (const [key, value] of Object.entries(pieces)) {
-    console.log(key, value);
     let cell = document.getElementById(value);
     switch (key) {
       /////////// BLACK PIECES
@@ -70,8 +87,8 @@ function setup() {
       case "blackTower2":
         cell.innerText = "♜";
         break;
-      case "blackHorse1":
-      case "blackHorse2":
+      case "blackKnight1":
+      case "blackKnight2":
         cell.innerText = "♞";
         break;
       case "blackBishop1":
@@ -133,16 +150,35 @@ setup();
 ////end of inititializing board
 
 //to select cell in boardContainer
-boardContainer.onclick = (e) => {
-  console.log(e.target.id); // get the element by id
-  var cell = document.getElementById(e.target.id);
+
+var cellStack = [];
+boardContainer.addEventListener("click", moveFunc);
+
+function moveFunc(event, cellStack) {
   try {
-    if (cell.innerText === "") {
-      cell.innerText = "♕";
-    } else {
-      cell.innerText = "";
+    var cell = event.target;
+    console.log(cell.id);
+    if (!cell.innerText === "") {
+      cellStack.push(cell);
+      console.log(cellStack);
     }
-  } catch {
-    //not a cell
-  }
-};
+  } catch {}
+  console.log(cellStack);
+}
+
+if (!cellStack.length === 1) {
+  let cell1 = cellStack[0];
+  let cell2 = cellStack[1];
+  let tempCellVal = cell2.innerText;
+
+  cell2.innerText = cell1.innerText;
+  cell1.innerText = tempCellVal;
+}
+
+// boardContainer.onclick = (e) => {
+//   console.log(e.target.id); // get the element by id
+//   //   var firstCell = document.getElementById(e.target.id);
+//   //   let firstVal = firstCell.innerText;
+//   //   document.title = e.target.id;
+//   //   console.log(firstVal);
+// };
